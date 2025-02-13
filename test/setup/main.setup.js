af68d6d982bce4b50f14473a3c238afe17359b06
Mike Maietta <mmaietta@outlook.com>
2025-02-12 18:24:22 -0800

    prettier

--- test/setup/main.setup.js
+++ test/setup/main.setup.js
@@ -3,10 +3,10 @@ const { app } = require('electron');
 if (app) {
   // needed to run electron-mocha within docker container as user is `root`.
   app.commandLine.appendSwitch('no-sandbox');
-//   app.commandLine.appendSwitch('headless');
+  //   app.commandLine.appendSwitch('headless');
   // Forces Electron not to start dbus
   // Resolves tests logging "Failed to connect to the bus: Failed to connect to socket /run/dbus/system_bus_socket: No such file or directory"
-//   app.commandLine.appendSwitch('remote-debugging-port', '9222');
+  //   app.commandLine.appendSwitch('remote-debugging-port', '9222');
   // resolves (unimpacting/noisy) logs of "viz_main_impl.cc(186)] Exiting GPU process due to errors during initialization"
   app.disableHardwareAcceleration();
 }
