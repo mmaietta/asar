1e1b3e1ab8eb38763585b8dfaf295a1b020d6247
Mike Maietta <mmaietta@outlook.com>
2025-02-19 17:57:03 -0800

    cleanup. tests pass. mount separate folder for docker node_modules

--- .mocharc.js
+++ .mocharc.js
@@ -2,7 +2,7 @@ module.exports = {
   'watch-files': ['test/**/*.js', 'lib/**/*.js'],
   recursive: true,
   reporter: 'spec',
-  "require-main": "test/setup/main.setup.js",
-  file: './mocha.setup.js', // setup file before everything else loads
+  "require-main": "./test/setup/main.setup.js",
+  file: './test/setup/mocha.setup.js', // setup file before everything else loads
   'forbid-only': process.env.CI ?? false, // make sure no `test.only` is merged into `main`
 };
